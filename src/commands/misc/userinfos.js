6f6c36b7fe677799e87fa11b3f7b96d8c95a7005
julien milants <julien.milants@gmail.com>
2021-11-10 10:55:35 +0100

    add deploy ; modifs userinfo

--- src/commands/misc/userinfos.js
+++ src/commands/misc/userinfos.js
@@ -3,15 +3,23 @@ const { Command } = require('discord-akairo');
 class UserInfoCommand extends Command {
     constructor() {
         super('userinfo', {
-            aliases: ['userinfo', 'info']
-            //description: 'Affiche les infos du user'//
-            
+            aliases: ['userinfo', 'info'],
+            // description: 'Affiche les infos du user'
+            //ignoreCooldown: '306841509314166785',
+            //ignorePermissions: '306841509314166785',
+            userPermissions: 'KICK_MEMBERS',
+            clientPermissions: 'KICK_MEMBERS',
+            //rateLimit: 1,
+            cooldown: 5000,
+            typing: true,
+            ownerOnly: true,
+            channel: 'guild',
         });
     }
 
-    exec(message) {
-        return message.reply('Salut');
-        }
+    exec(message){
+        return message.reply('salut !')
+    }
 }
 
 module.exports = UserInfoCommand;
